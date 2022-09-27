74c6627e2ad2d9311453a82498d58a99b000fc8c
wesoree <wesleylo174@gmail.com>
2022-09-27 14:00:39 -0400

    create pinewood.py

---
+++ misc/pinewood.py
@@ -0,0 +1,13 @@
+mypoints = input("how many points")
+mypoints = int(mypoints)
+
+if mypoints == 21:
+    ans = input('you\'re stupid, what\'s 9+10?')
+    ans = int(ans)
+    if ans != 19:
+        print("you're stupid")
+    else:
+        print("ok")
+
+else:
+    print ("you have", mypoints, "points")
