48c6374b92913bba12926e9cd91ba1e50d0d7e22
wesoree <wesleylo174@gmail.com>
2022-10-03 13:28:25 -0400

    update pinewood.py

--- misc/pinewood.py
+++ misc/pinewood.py
@@ -4,10 +4,11 @@ mypoints = int(mypoints)
 if mypoints == 21:
     ans = input('you\'re stupid, what\'s 9+10?')
     ans = float(ans)
-    if ans != 19:
+    if ans == 19:
+        print("ok")
+    elif ans == 21:
         print("you're stupid")
     else:
-        print("ok")
-
+        print("why")
 else:
     print ("you have", mypoints, "points")
