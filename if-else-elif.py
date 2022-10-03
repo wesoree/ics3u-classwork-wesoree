4d7e06f57c02c384757639722fa16ed4f97b3cbb
wesoree <wesleylo174@gmail.com>
2022-10-03 14:33:15 -0400

    Update if-else-elif.py

--- if-else-elif.py
+++ if-else-elif.py
@@ -1,1 +1,7 @@
-print ('hello world')
+name = input("what's your name")
+print("hello, " + name +)
+age = input("how old are you?")
+age = int(age)
+if age < 12:
+    print('you can\'t use Discord')
+    print("!ban ")
