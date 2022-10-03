230231f704fab2876d66d64242a1027c6f65e5f9
wesoree <75049633+wesoree@users.noreply.github.com>
2022-10-03 18:14:17 -0400

    update if-else-elif.py

--- if-else-elif.py
+++ if-else-elif.py
@@ -1,7 +1,23 @@
 name = input("what's your name")
-print("hello, " + name +)
+print("hello, " + name)
 age = input("how old are you?")
 age = int(age)
-if age < 12:
+if age <= 12:
     print('you can\'t use Discord')
-    print("!ban ")
+    print("!ban " + name)
+elif age == 13:
+    print("you can use Discord")
+elif age == 16:
+    print("you can drive")
+elif age == 18:
+    print("you can vote")
+elif age == 21:
+    ans = input('you\'re stupid, what\'s 9+10?')
+    ans = int(ans)
+    if ans ==21:
+        print("you're stupid")
+    elif ans ==19:
+        print("you can rent a car")
+    else:
+        print("!ban" + name)
+    
