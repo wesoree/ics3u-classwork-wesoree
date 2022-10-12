56e53bf2e002c2b727d9018adf281702e15c7751
Wesley Lo <wesleylo174@gmail.com>
2022-10-12 13:57:37 -0400

    update 9671111.py

--- 9671111.py
+++ 9671111.py
@@ -1,7 +1,7 @@
 age=input('how old are you?')
 age=int(age)
 
-if age <= 5:
+if age >= 5:
     height=input('how tall are you?')
     height=int(height)
     if height == 21:
@@ -11,7 +11,7 @@ if age <= 5:
             print ('you stupid')
         elif ans == 19:
             print("goodbye")
-    elif height <= 100:
+    elif height >= 100:
         print("you can ride")
     else:
         print("you cannot ride")
