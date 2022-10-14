d462fb72e9ba4a9ae48557d4737fc6ba90131f49
Wesley Lo <wesleylo174@gmail.com>
2022-10-14 14:10:25 -0400

    update

--- landscape.py
+++ landscape.py
@@ -1,3 +1,13 @@
 import pygame
 from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
 
+pygame.init()
+
+WIDTH = 640
+HEIGHT = 480
+SIZE = (WIDTH,HEIGHT)
+
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+
+pygame.draw
