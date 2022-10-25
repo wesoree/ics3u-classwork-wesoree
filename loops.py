c88d80a43f790deb19b9ee304447e15c8585e3eb
wesoree <wesleylo174@gmail.com>
2022-10-25 13:40:43 -0400

    create

---
+++ loops.py
@@ -0,0 +1,10 @@
+import random
+x = random.randrange(1,15)
+
+while True:
+    guess = input("pick number between 1 to 15")
+    guess = int(guess)
+    if guess == x:
+        print ("guess is correct")
+        break
+
