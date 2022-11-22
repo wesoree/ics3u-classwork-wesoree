d7a83fce4b25e358116fed7101355d9f4197b07c
wesoree <wesleylo174@gmail.com>
2022-11-22 12:35:36 -0500

    mod repo to prevent explosions

--- demos/9671111.py
+++ demos/9671111.py
@@ -1,19 +1,33 @@
-age=input('how old are you?')
-age=int(age)
-
-if age >= 5:
-    height=input('how tall are you?')
-    height=int(height)
-    if height == 21:
-        ans=input('you stupid, what\'s 9+10?')
-        ans=int(ans)
-        if ans == 21:
-            print ('you stupid')
-        elif ans == 19:
-            print("goodbye")
-    elif height >= 100:
-        print("you can ride")
+while True:
+    try:
+        age=int(input('how old are you?'))
+    except ValueError:
+        print("invalid age, please try again")
+        age = 1000
+        break
+    if age != 0:
+        break
+while age != 1000:
+    if age >= 5:
+        try:
+            height=float(input('how tall are you?'))
+        except ValueError:
+            print("invalid height, please try again")
+            break
+        if height == 21:
+            ans=input('you stupid, what\'s 9+10?')
+            ans=int(ans)
+            if ans == 21:
+                print ('you stupid')
+                break
+            elif ans == 19:
+                print("goodbye")
+                break
+        elif height >= 100:
+            print("you can ride")
+            break
+        else:
+            print("you cannot ride bc u too short")
+            break
     else:
-        print("you cannot ride bc u too short")
-else:
-    print("you cannot ride bc underage")
+        print("you cannot ride bc underage")
